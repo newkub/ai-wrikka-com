c30d0ac1d8b989a9a3bc713fb926b98740c35828
newkub <newkubise@gmail.com>
2025-06-02 18:17:58 +0700

    chore(config): update configuration files and documentation

--- uno.config.ts
+++ uno.config.ts
@@ -72,11 +72,4 @@ export default defineConfig({
 			linear: "var(--transition-timing-linear)",
 		},
 	},
-	getCSS: ({ theme }) => `
-      * {
-        color: ${theme.colors.gray?.[700] ?? "#333"};
-        padding: 50;
-        margin: 0;
-      }
-    `,
 });
