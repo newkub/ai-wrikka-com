bfaf5b1574c5fcc5e1af575615f215c799b3520b
newkub <newkubise@gmail.com>
2025-06-07 23:12:10 +0700

    chore: remove unused components

--- components/Card.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="rounded-lg border border-border bg-background p-4 shadow-sm hover:shadow-md transition-shadow duration-200">
-    <slot></slot>
-  </div>
-</template>
-
-<script setup lang="ts">
-// Card component with basic styling
-</script>
